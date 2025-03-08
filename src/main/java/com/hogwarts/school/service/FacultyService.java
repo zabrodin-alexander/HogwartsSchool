9e1290d117b431c9f78f7b7b4978748defd6e729
Zabro <zabrodin.alexander51@gmail.com>
2025-03-08 12:32:06 +0300

    begin

---
+++ src/main/java/com/hogwarts/school/service/FacultyService.java
@@ -0,0 +1,30 @@
+package com.hogwarts.school.service;
+
+import com.hogwarts.school.model.Faculty;
+import com.hogwarts.school.repository.FacultyRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FacultyService {
+    private final FacultyRepository facultyRepository;
+
+    public FacultyService(FacultyRepository facultyRepository) {
+        this.facultyRepository = facultyRepository;
+    }
+
+    public Faculty addFaculty(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public Faculty findFaculty(long id) {
+        return facultyRepository.findById(id).get();
+    }
+
+    public Faculty editFaculty(Faculty faculty) {
+        return facultyRepository.save(faculty);
+    }
+
+    public void deleteFaculty(long id) {
+        facultyRepository.deleteById(id);
+    }
+}
